85a0c44f7c5e61fc6cd65f7275d52f2dd958f516
Krystof Tulinger <k.tulinger@seznam.cz>
2017-10-31 08:49:52 +0100

    correcting the path for composer

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /app && \
-#php composer.phar --no-interaction install && \
-php -S 0.0.0.0:80 -t . && \
+php /usr/bin/composer.phar --no-interaction install && \
+php -S 0.0.0.0:80 -t www && \
 echo "Serving on 0.0.0.0:80"
